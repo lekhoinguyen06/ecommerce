d249febaab7956e161e3a5207b799715b722505a
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-01-31 16:59:55 +0700

    feat(auth): match validator

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,11 +1,13 @@
 import { Exclude } from 'class-transformer';
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
+import { Match } from 'src/shared/decorators/custom-validator.decorator';
 
 export class LoginBodyDTO {
   // Should provide uuid for token uniqueness
   @IsString()
   email: string;
   @IsString()
+  @Length(8, 20, { message: 'Password must be between 8 to 20 charactors' })
   password: string;
 }
 
@@ -13,6 +15,7 @@ export class RegisterBodyDTO extends LoginBodyDTO {
   @IsString()
   name: string;
   @IsString()
+  @Match('password', { message: 'Password must match' })
   confirmPassword: string;
 }
 
