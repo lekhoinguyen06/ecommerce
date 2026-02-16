fbf0e4c7ecc41b028dbe395fc9832728285f4374
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-02-16 22:52:15 +0700

    feat(email): verification code ui

--- src/shared/services/email.service.tsx
+++ src/shared/services/email.service.tsx
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Resend } from 'resend';
 import envConfig from '../config';
+import { LinearLoginCodeEmail } from 'emails/verification-code.email';
 
 @Injectable()
 export class EmailService {
@@ -13,7 +14,7 @@ export class EmailService {
       from: 'onboarding@resend.dev',
       to: 'khoinguyenbenz@gmail.com',
       subject: 'Your verification code',
-      html: `<p>Congrats on sending your <strong>first email</strong>! Your account registration OTP code is <h3>${payload.code}</h3></p>`,
+      react: <LinearLoginCodeEmail validationCode={payload.code} />,
     });
   }
 }
