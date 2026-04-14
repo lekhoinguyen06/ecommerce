a16142361c889ed0b00a1a1a3ad82f0a0e68a8d6
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-04-14 16:58:15 +0700

    feat: add permission schemas

---
+++ src/routes/permission/permission.model.ts
@@ -0,0 +1,62 @@
+import { HTTPMethod } from 'src/shared/constants/permission.constant';
+import z from 'zod';
+
+export const PermissionSchema = z.object({
+  id: z.number().int(),
+  name: z.string(),
+  description: z.string().default(''),
+  path: z.string(),
+  method: z.enum(HTTPMethod),
+
+  createdById: z.number().int().nullable(),
+  updatedById: z.number().int().nullable(),
+  deletedById: z.number().int().nullable(),
+
+  deletedAt: z.date().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+// QUERY
+export const GetPermissionsBodySchema = z
+  .object({
+    page: z.coerce.number().int().default(1),
+    limit: z.coerce.number().int().default(10),
+  })
+  .strict();
+
+export const GetPermissionsParamSchema = z
+  .object({
+    permissionId: z.coerce.number().int(),
+  })
+  .strict();
+
+export const GetPermissionResSchema = z.object({
+  data: z.array(PermissionSchema),
+  totalItems: z.number().int(),
+  totalPages: z.number().int(),
+  currentPage: z.number().int(),
+  limit: z.number().int(),
+});
+
+// CREATE
+export const CreatePermissionBodySchema = PermissionSchema.pick({
+  name: true,
+  path: true,
+  method: true,
+}).strict();
+
+// UPDATE
+export const UpdatePermissionBodySchema = CreatePermissionBodySchema;
+
+// Types
+export type Permission = z.infer<typeof PermissionSchema>;
+export type GetPermissionsBodyType = z.infer<typeof GetPermissionsBodySchema>;
+export type GetPermissionsParamType = z.infer<typeof GetPermissionsParamSchema>;
+export type GetPermissionResType = z.infer<typeof GetPermissionResSchema>;
+export type CreatePermissionBodyType = z.infer<
+  typeof CreatePermissionBodySchema
+>;
+export type UpdatePermissionBodyType = z.infer<
+  typeof UpdatePermissionBodySchema
+>;
---
+++ src/shared/constants/permission.constant.ts
@@ -0,0 +1,9 @@
+export const HTTPMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+} as const;
+
+export type HTTPMethodType = (typeof HTTPMethod)[keyof typeof HTTPMethod];
