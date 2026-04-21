6434d98d3fcd2ac069bce687a8b8f64ff63da518
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-04-21 15:36:19 +0700

    chore: reapply unique constraint migrations

--- prisma/migrations/20260421083221_apply_unique_constraints/migration.sql
+++ prisma/migrations/20260421083221_apply_unique_constraints/migration.sql
@@ -1,7 +1,7 @@
 CREATE UNIQUE INDEX permission_path_method_unique 
-ON "Permission" ("path", "method")
+ON "Permission"("path", "method")
 WHERE "deletedAt" IS NULL;
 
-CREATE UNIQUE INDEX role_name_unique
-ON "Role" ("name")
+CREATE UNIQUE INDEX role_name_unique 
+ON "Role"("name")
 WHERE "deletedAt" IS NULL;
