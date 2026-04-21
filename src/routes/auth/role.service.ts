9d2cf3b7b6bc2b4d73aac1a761e24a7c5d222e76
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-04-21 16:04:27 +0700

    fix: integrate schema change with role service

--- src/routes/auth/role.service.ts
+++ src/routes/auth/role.service.ts
@@ -10,11 +10,17 @@ export class RoleService {
 
   async getClientRoleId() {
     if (this.clientRoleId) return this.clientRoleId;
-    const role = await this.prismaService.role.findFirstOrThrow({
+    const role = await this.prismaService.role.findFirst({
       where: {
         name: RoleName.Client,
+        deletedAt: null,
       },
     });
+
+    if (!role) {
+      throw new Error(`Role with name ${RoleName.Client} not found`);
+    }
+
     this.clientRoleId = role.id;
     return this.clientRoleId;
   }
