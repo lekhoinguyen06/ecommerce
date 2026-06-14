e4b2a9e8bfdaf02669e4724fb17156af6197af5b
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-14 11:15:30 +0700

    feat(media): add file validator

--- src/routes/media/media.controller.ts
+++ src/routes/media/media.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -9,8 +10,44 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('media')
 export class MediaController {
   @Post('images/upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: {
+        // VALIDATOR: Limit file size to 5 MB
+        fileSize: 5 * 1024 * 1024, // 5 MB
+      },
+      fileFilter(req, file, callback) {
+        // VALIDATOR: Is the file an image?
+        if (!file.mimetype.startsWith('image/')) {
+          return callback(
+            new BadRequestException('Only image files are allowed!'),
+            false,
+          );
+        }
+
+        // VALIDATOR: Does the file have an allowed extension?
+        const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        if (
+          !allowedExtensions.some((ext) =>
+            file.originalname.toLowerCase().endsWith(ext),
+          )
+        ) {
+          return callback(
+            new BadRequestException(
+              'Only jpg, jpeg, png, and gif files are allowed!',
+            ),
+            false,
+          );
+        }
+
+        callback(null, true);
+      },
+    }),
+  )
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
     console.log(file);
   }
 }
