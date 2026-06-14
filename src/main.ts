7d93280800b16d779692b264347ded0b4a8b4f78
lekhoinguyen <khoinguyenbenz@gmail.com>
2026-06-14 11:58:45 +0700

    feat(media): guard static serve

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { UPLOAD_PATH } from './shared/constants/media.constant';
 import envConfig from './shared/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  app.useStaticAssets(UPLOAD_PATH, {
-    prefix: envConfig.STATIC_PREFIX,
-  });
   await app.listen(envConfig.APP_PORT ?? 3000);
 }
 bootstrap();
--- src/routes/media/media.controller.ts
+++ src/routes/media/media.controller.ts
@@ -1,12 +1,18 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  NotFoundException,
+  Param,
   Post,
+  Res,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import type { Response } from 'express';
 import envConfig from 'src/shared/config';
+import { UPLOAD_PATH } from 'src/shared/constants/media.constant';
 
 @Controller('media')
 export class MediaController {
@@ -56,5 +62,18 @@ export class MediaController {
         url: `${envConfig.STATIC_ENDPOINT}/${file.filename}`,
       })),
     };
+  }
+
+  @Get('static/:filename')
+  serveStaticFile(@Param('filename') filename: string, @Res() res: Response) {
+    return res.sendFile(filename, { root: UPLOAD_PATH }, (err) => {
+      if (err) {
+        const FileNotFoundException = new NotFoundException('File not found');
+        res.status(FileNotFoundException.getStatus()).json({
+          statusCode: FileNotFoundException.getStatus(),
+          message: FileNotFoundException.message,
+        });
+      }
+    });
   }
 }
